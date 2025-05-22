def39fc9c5ab9b1bd8d6254ddd01d23d9b2c3263
Thomas V Varghese <118618681+www-domain@users.noreply.github.com>
2025-05-22 20:21:03 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,24 @@ resource "aws_subnet" "subnet" {
   map_public_ip_on_launch = true
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.rt.id
+}
+
 resource "aws_security_group" "backend_sg" {
   vpc_id = aws_vpc.my_vpc.id
 
