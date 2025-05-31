fb4ffb1f269b5c9127637051ebc35ea4226a8929
Thomas V Varghese <118618681+www-domain@users.noreply.github.com>
2025-05-31 09:55:24 +0530

    Update data.tf

--- VPC/data.tf
+++ VPC/data.tf
@@ -1,1 +1,8 @@
-
+data "terraform_remote_state" "s3" {
+  backend = "s3"
+  config = {
+    bucket = var.tf_state_bucket
+    key    = "env:/${var.environment}/s3/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
