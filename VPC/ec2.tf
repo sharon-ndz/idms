3783e7407aaffc170ec7d709a39c81f5d6e5e47c
Thomas V Varghese <118618681+www-domain@users.noreply.github.com>
2025-05-31 10:40:33 +0530

    Update ec2.tf

--- VPC/ec2.tf
+++ VPC/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0c94855ba95c71c99" # Replace with correct AMI for your region
+  ami           = "ami-03a13a09a711d3871" # Replace with correct AMI for your region
   instance_type = var.instance_type
 
-  subnet_id              = module.vpc.public_subnets[0]
+  subnet_id              = module.vpc.private_app_subnets[0]
   vpc_security_group_ids = [module.vpc.default_security_group_id]
 
   tags = {
