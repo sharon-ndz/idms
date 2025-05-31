1a1e29f3667bd9b0b197f39e7ad33342f48a68eb
sharon-ndz <sharon@ndz.co>
2025-05-31 15:34:22 +0530

    Update network.tf

--- VPC/network.tf
+++ VPC/network.tf
@@ -1,19 +1,1 @@
-resource "aws_lb" "backend_nlb" {
-  name               = "backend-nlb"
-  internal           = true  # Private within the VPC
-  load_balancer_type = "network"
-  subnets            = var.private_app_subnets.cidrs_blocks
-}
 
-resource "aws_lb_target_group" "backend_target_group" {
-  name        = "backend-target-group"
-  port        = 80
-  protocol    = "TCP"
-  vpc_id      = module.vpc.vpc_id
-  target_type = "instance"
-}
-
-resource "aws_lb_target_group_attachment" "backend_instance_attachment" {
-  target_group_arn = aws_lb_target_group.backend_target_group.arn
-  target_id        = aws_instance.web.id
-}
