4fd223edbae840f4ad56c289faddd50a40f62428
Sharon Biju <sharon.biju@gate39media.com>
2025-06-01 12:18:51 +0530

    IAM role added

--- VPC/ec2.tf
+++ VPC/ec2.tf
@@ -1,11 +1,26 @@
 resource "aws_instance" "web" {
+<<<<<<< HEAD
   ami           = "ami-0f9de6e2d2f067fca" # Replace with correct AMI for your region
+=======
+
+>>>>>>> 5213a0f (IAM role added)
   instance_type = var.instance_type
 
   subnet_id              = module.vpc.public_subnets_ids[0]
   vpc_security_group_ids = [module.vpc.default_security_group_id]
-
+  iam_instance_profile   = aws_iam_instance_profile.ec2_ssm_profile.name
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install -y amazon-ssm-agent
+              sudo systemctl enable amazon-ssm-agent
+              sudo systemctl start amazon-ssm-agent
+              EOF
   tags = {
+<<<<<<< HEAD
     Name = "Backend API-Docker"
+=======
+    Name = "Backend API2"
+>>>>>>> 5213a0f (IAM role added)
   }
 }
