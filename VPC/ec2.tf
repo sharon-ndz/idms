1a860af75373a95b73a9b9ecc57fb055b1e7b699
sharon-ndz <sharon@ndz.co>
2025-06-01 11:04:29 +0530

    Update ec2.tf

--- VPC/ec2.tf
+++ VPC/ec2.tf
@@ -1,11 +1,12 @@
 resource "aws_instance" "web" {
-  ami           = "ami-03a13a09a711d3871" # Replace with correct AMI for your region
+  ami           = "ami-0f9de6e2d2f067fca" # Replace with corr
+ect AMI for your region
   instance_type = var.instance_type
 
   subnet_id              = module.vpc.public_subnets_ids[0]
   vpc_security_group_ids = [module.vpc.default_security_group_id]
 
   tags = {
-    Name = "Backend API"
+    Name = "Backend API-Docker"
   }
 }
