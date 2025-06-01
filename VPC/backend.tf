c8e0075a41b632a51439e1541f6f3add96f5a6b8
Sharon Biju <sharon.biju@gate39media.com>
2025-06-01 07:50:46 +0530

    terraform changed

--- VPC/backend.tf
+++
@@ -1,6 +0,0 @@
-
-terraform {
-  backend "s3" {
-    key = "vpc/terraform.tfstate"
-  }
-}
--- VPC/data.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "s3" {
-  backend = "s3"
-  config = {
-    bucket = var.tf_state_bucket
-    key    = "env:/${var.environment}/s3/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
